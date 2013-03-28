5bb074f67fc9a133cbb3be43e09d7a4821eb9a0c
zhon <zhon@xputah.org>
2013-03-27 20:00:15 -0600

    Refactor classes into module

--- lib/vim_sitter/vim_dir.rb
+++ lib/vim_sitter/vim_dir.rb
@@ -1,41 +1,43 @@
 require 'fileutils'
 
-class VimDir
-  # TODO need 
-  # linux
-  # mac
+module VimSitter
+  class VimDir
+    # TODO need 
+    # linux
+    # mac
 
-  def create_bundle
-    bundle_dir = "#{base_dir}/bundle"
-    FileUtils.mkdir_p(bundle_dir)
+    def create_bundle
+      bundle_dir = "#{base_dir}/bundle"
+      FileUtils.mkdir_p(bundle_dir)
+    end
+
+    def create_autoload
+      FileUtils.mkdir_p(base_dir + '/autoload')
+    end
+
+    def create_swap
+      FileUtils.mkdir_p(data_dir + '/swap')
+    end
+
+    def create_backup
+      FileUtils.mkdir_p(data_dir + '/backup')
+    end
+
+    def create_undo
+      FileUtils.mkdir_p(data_dir + '/undo')
+    end
+
+    def data_dir
+      ENV['AppData'].gsub('\\','/') + '/Vim'
+    end
+
+    def base_dir
+      File.expand_path("~/#{windows? ? 'vimfiles' : '.vim'}")
+    end
+
+    def windows?
+      RUBY_PLATFORM =~ /(win|w)32$/
+    end
+
   end
-
-  def create_autoload
-    FileUtils.mkdir_p(base_dir + '/autoload')
-  end
-
-  def create_swap
-    FileUtils.mkdir_p(data_dir + '/swap')
-  end
-
-  def create_backup
-    FileUtils.mkdir_p(data_dir + '/backup')
-  end
-
-  def create_undo
-    FileUtils.mkdir_p(data_dir + '/undo')
-  end
-
-  def data_dir
-    ENV['AppData'].gsub('\\','/') + '/Vim'
-  end
-
-  def base_dir
-    File.expand_path("~/#{windows? ? 'vimfiles' : '.vim'}")
-  end
-
-  def windows?
-    RUBY_PLATFORM =~ /(win|w)32$/
-  end
-
 end
